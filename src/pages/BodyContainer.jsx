dcb72e400f21c31a6fb8da6cf51085d431bc48c2
Laxminarayan Majhi <laxminarayanmajhi@Laxminarayans-Mac-mini.local>
2023-06-21 00:33:11 +0530

    Was able to highlight a squaer area on the map, based on co-ordinates.

--- src/pages/BodyContainer.jsx
+++ src/pages/BodyContainer.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import {
+  Map,
+  InfoWindow,
+  Marker,
+  GoogleApiWrapper,
+  Polygon,
+} from "google-maps-react";
 const YOUR_GOOGLE_API_KEY_GOES_HERE = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
 //sample API key AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo to be used lightly
 
@@ -12,9 +18,9 @@ class BodyContainer extends Component {
   onMarkerClick(props, marker, e) {
     console.log(
       "🚀 ~ file: BodyContainer.jsx:13 ~ BodyContainer ~ onMarkerClick ~ props, marker, e:",
-      props,
-      marker,
-      e
+      { props },
+      { marker },
+      { e }
     );
   }
   render() {
@@ -26,6 +32,35 @@ class BodyContainer extends Component {
       lng: 84.7417494,
     };
 
+    const odishaBounds = [
+      {
+        lat: 22.5,
+        lng: 87.5,
+      },
+      {
+        lat: 22.5,
+        lng: 81.2,
+      },
+
+      {
+        lat: 18.0,
+        lng: 81.2,
+      },
+      {
+        lat: 18.0,
+        lng: 87.5,
+      },
+    ];
+
+    var bounds = new this.props.google.maps.LatLngBounds();
+    for (var i = 0; i < odishaBounds.length; i++) {
+      bounds.extend(odishaBounds[i]);
+    }
+    console.log(
+      "🚀 ~ file: BodyContainer.jsx:49 ~ BodyContainer ~ render ~ bounds:",
+      bounds
+    );
+
     return (
       <div>
         <Map
@@ -33,6 +68,7 @@ class BodyContainer extends Component {
           google={this.props.google}
           zoom={10}
           initialCenter={givenPosition}
+          bounds={bounds}
         >
           <Marker
             title={"The marker`s title will appear as a tooltip."}
@@ -40,7 +76,14 @@ class BodyContainer extends Component {
             name={"Current location"}
             position={givenPosition}
           />
-
+          <Polygon
+            paths={odishaBounds}
+            strokeColor="#0000FF"
+            strokeOpacity={0.8}
+            strokeWeight={2}
+            fillColor="red"
+            fillOpacity={0.35}
+          />
           <InfoWindow onClose={this.onInfoWindowClose}>
             <div>{<h1>Hello</h1>}</div>
           </InfoWindow>
@@ -51,6 +94,6 @@ class BodyContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: YOUR_GOOGLE_API_KEY_GOES_HERE,
+  // apiKey: YOUR_GOOGLE_API_KEY_GOES_HERE,
   // LoadingContainer: LoadingContainer,
 })(BodyContainer);
