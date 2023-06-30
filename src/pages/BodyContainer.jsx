5907784bde76f53da9198c5c3ec6d400f71ecb96
Laxminarayan Majhi <laxminarayanmajhi@Laxminarayans-Mac-mini.local>
2023-07-01 00:13:16 +0530

    Lot many changes.

--- src/pages/BodyContainer.jsx
+++ src/pages/BodyContainer.jsx
@@ -1,103 +1,105 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper, Circle } from "google-maps-react";
-import districts from "./districts.json";
-import { odishaBounds } from "./locations";
-import districtDetails from "./districtDetails.json";
-import {
-  AreaChartOutlined,
-  TeamOutlined,
-  DotChartOutlined,
-} from "@ant-design/icons";
+import { Map, GoogleApiWrapper, Circle, Marker } from "google-maps-react";
+import dayjs from "dayjs";
+
 const YOUR_GOOGLE_API_KEY_GOES_HERE = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
-import { Card, Image } from "antd";
+import { Card, Image, DatePicker, Col, Row, Space } from "antd";
+import restClient from "../utils/restClient";
 //sample API key AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo to be used lightly
 
+const todaysDate = () => {
+  let currentDate = dayjs(new Date());
+
+  return currentDate;
+};
 const LoadingContainer = (props) => <div>Fancy loading container!</div>;
 class BodyContainer extends Component {
   constructor(props) {
     super(props);
-    this.onMarkerClick = this.onMarkerClick.bind(this);
+
+    this.onMapClick = this.onMapClick.bind(this);
+    this.onFromDateChange = this.onFromDateChange.bind(this);
+    this.onToDateChange = this.onToDateChange.bind(this);
+
     this.state = {
-      selectedDistrict: {},
+      selectLatLong: {},
+      apiResponse: {},
+      toDate: todaysDate(),
+      fromDate: todaysDate(),
     };
   }
-
-  onMarkerClick = (props, district) => {
-    console.log("props", props);
-    console.log("district", district);
-    const tempDistrictItem = districtDetails.find(
-      (item) => item.name === district.district
-    );
+  onFromDateChange = (date, dateString) => {
     this.setState({
-      selectedDistrict: tempDistrictItem,
+      fromDate: dateString,
+    });
+  };
+
+  onToDateChange = (date, dateString) => {
+    this.setState({
+      toDate: dateString,
+    });
+  };
+  onMapClick = async (props, someMore, more) => {
+    this.setState({
+      selectLatLong: {
+        lat: more.latLng.lat(),
+        lng: more.latLng.lng(),
+      },
+    });
+    const response = await restClient.post("/mapIndicator", {
+      lat: more.latLng.lat(),
+      long: more.latLng.lng(),
+      radius: 1000,
+      from_date: this.state.fromDate,
+      to_date: this.state.toDate,
+      indicator: "flood",
+    });
+    /**
+     * { "status": true, "latitude": 12.991630005, "longitude": 77.5088097141, "floodIndicator": "1.0000" }
+     */
+    this.setState({
+      ...this.state,
+
+      apiResponse: response.data,
     });
   };
 
   render() {
     const givenPosition = {
-      // odisha co-ordinates
-      //22.2132362,84.7417494
-      lat: 22.2132362,
-      lng: 84.7417494,
+      //bengaluru https://www.google.com/maps/place/Bengaluru,+Karnataka/@12.9537902,77.3012721
+      lat: 12.9537902,
+      lng: 77.3012721,
     };
 
-    var bounds = new this.props.google.maps.LatLngBounds();
-    for (var i = 0; i < odishaBounds.length; i++) {
-      bounds.extend(odishaBounds[i]);
-    }
-
     return (
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div
-          style={{
-            display: "flex",
-            width: "90vw",
-
-            height: "80vh",
-          }}
-        >
-          <Map
-            style={{ /* margin: 20, */ width: "70vw", height: "80vh" }}
-            google={this.props.google}
-            zoom={6}
-            initialCenter={givenPosition}
-            bounds={bounds}
-          >
-            {/* {districts.map((district) => (
-              <Marker
-                title={district.district}
-                name={district.district}
-                position={{ lat: district.latitude, lng: district.longitudes }}
-                // onClick={this.onMarkerClick}
+      <>
+        <Row>
+          <Col span={18}>
+            <Space direction="horizontal">
+              From:{" "}
+              <DatePicker
+                defaultValue={this.state.fromDate}
+                onChange={this.onFromDateChange}
               />
-            ))} */}
-
-            {districts.map((district) => (
-              <Circle
-                center={{ lat: district.latitude, lng: district.longitude }}
-                strokeColor={"red"}
-                strokeOpacity={district.population_density / 1000}
-                strokeWeight={0.8}
-                fillColor={"red"}
-                fillOpacity={district.population_density / 1000}
-                radius={28000}
-                onClick={(e) => this.onMarkerClick(e, district)}
+              <p></p>
+              To:{" "}
+              <DatePicker
+                defaultValue={this.state.toDate}
+                onChange={this.onToDateChange}
               />
-            ))}
-          </Map>
-        </div>
-        <div
-          style={{
-            display: "flex",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
+            </Space>
 
-              float: "right",
-            }}
-          >
+            <Map
+              style={{ width: "70vw", height: "80vh", marginTop: 20 }}
+              google={this.props.google}
+              zoom={11}
+              initialCenter={givenPosition}
+              onClick={this.onMapClick}
+            >
+              <Marker position={this.state.selectLatLong} />
+            </Map>
+          </Col>
+          <Col span={6}>
             <Card
               title={this.state.selectedDistrict?.name}
               style={{
@@ -105,33 +107,15 @@ class BodyContainer extends Component {
                 height: 300,
               }}
             >
-              {Object.keys(this.state.selectedDistrict).length > 0 ? (
+              {Object.keys(this.state.apiResponse).length > 0 ? (
                 <>
-                  <p>
-                    <TeamOutlined /> Population :{" "}
-                    {this.state.selectedDistrict.population}
-                  </p>
-                  <p>
-                    <AreaChartOutlined /> Area:{" "}
-                    {this.state.selectedDistrict.area} (/km
-                    <span style={{ verticalAlign: "super" }}>2</span>)
-                  </p>
-                  <p>
-                    <DotChartOutlined />
-                    Rainfall: {this.state.selectedDistrict.averageRainfall}
-                  </p>
-                  {/* {this.state.selectedDistrict.image && (
-                    <Image
-                      width={200}
-                      src={this.state.selectedDistrict.image}
-                    />
-                  )} */}
+                  <p>{JSON.stringify(this.state.apiResponse, null, 2)}</p>
                 </>
               ) : null}
             </Card>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
